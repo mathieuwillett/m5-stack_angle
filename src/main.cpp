c7de66e5605d281d78690a3ff6aaf8a63f56dd94
mathieuwillett <143769896+mathieuwillett@users.noreply.github.com>
2025-09-02 10:28:31 -0400

    commit opacite

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,26 @@
 
 #define MA_BROCHE_ANGLE 32
 
+#include <FastLED.h>
+
+CRGB pixelAtom;
+
 void setup() {
   Serial.begin(115200);
+  FastLED.addLeds<WS2812, 27, GRB>(&pixelAtom, 1); 
 }
 
 void loop() {
   int maLectureAngle = analogRead(MA_BROCHE_ANGLE);
 
-  Serial.println(maLectureAngle);
+  Serial.println(maLectureAngle); 
+
+  if (maLectureAngle > 2050) {
+    pixelAtom = CRGB(255,0,0); 
+  } else {
+    pixelAtom = CRGB(0,255,0);
+  }
+
+  FastLED.show();
   delay(200);
 }
